40624fb1973849e935216f84b7ec947c66a19ce6
felix <felix@lagarden.de>
2020-03-11 12:09:21 +0100

    started implementation of data splitter

--- data_organiser.py
+++ data_organiser.py
@@ -7,3 +7,16 @@ data_arrival = pq.read_table("data/arrival_dataset.parquet")
 
 data_arrival = data_arrival.to_pandas()
 #data_departure = data_departure.to_pandas()
+
+flights = set(data_arrival["callsign"])
+
+print("Found", len(flights), "different aircraft for arrival")
+for flight in flights:
+    temp = pd.DataFrame(columns=data_arrival.columns)
+    indices = data_arrival[data_arrival["callsign"] == flight].index.tolist()
+    print(len(indices))
+    print(flight, data_arrival.iloc[indices[0]]["callsign"])
+    #for row in data_arrival.row:
+    #    if row["callsign"] == flight:
+    #        temp.append(row)
+
