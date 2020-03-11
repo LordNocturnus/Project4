f0047c08f3e25e0b8766e66b0c41286a6c7f9118
LordNocturnus <41686329+LordNocturnus@users.noreply.github.com>
2020-03-11 18:03:12 +0100

    finished data splitter

--- data_organiser.py
+++ data_organiser.py
@@ -1,22 +1,32 @@
 import numpy as np
 import pandas as pd
 import pyarrow.parquet as pq
+import os
+
 
 data_arrival = pq.read_table("data/arrival_dataset.parquet")
-#data_departure = pq.read_table("data/departure_dataset.parquet")
-
 data_arrival = data_arrival.to_pandas()
-#data_departure = data_departure.to_pandas()
-
 flights = set(data_arrival["callsign"])
 
 print("Found", len(flights), "different aircraft for arrival")
-for flight in flights:
-    temp = pd.DataFrame(columns=data_arrival.columns)
-    indices = data_arrival[data_arrival["callsign"] == flight].index.tolist()
-    print(len(indices))
-    print(flight, data_arrival.iloc[indices[0]]["callsign"])
-    #for row in data_arrival.row:
-    #    if row["callsign"] == flight:
-    #        temp.append(row)
+if "arrival_processed" not in os.listdir(os.getcwd() + "\\data"):
+    os.mkdir("data\\arrival_processed")
+for flight in list(flights):
+    print(flight, list(flights).index(flight) / len(flights))
+    temp = data_arrival[data_arrival["callsign"] == flight]
+    temp = temp.sort_values("timestamp")
+    temp.to_csv(f'data\\arrival_processed\\{flight}.csv', index=False)
 
+del data_arrival
+data_departure = pq.read_table("data/departure_dataset.parquet")
+data_departure = data_departure.to_pandas()
+flights = set(data_arrival["callsign"])
+
+print("Found", len(flights), "different aircraft for departure")
+if "departure_processed" not in os.listdir(os.getcwd() + "\\data"):
+    os.mkdir("data\\departure_processed")
+for flight in flights:
+    print(flight, list(flights).index(flight) / len(flights))
+    temp = data_arrival[data_departure["callsign"] == flight]
+    temp = temp.sort_values("timestamp")
+    temp.to_csv(f'data\\departure_processed\\{flight}.csv', index=False)
