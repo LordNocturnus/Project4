33296742fc27640d77a4e265068f5b6d021c1184
Jelmer <jelmer.dehaan10@gmail.com>
2020-03-24 18:37:03 +0100

    Implemented flight importer

--- runway_analyser.py
+++ runway_analyser.py
@@ -1,1 +1,20 @@
 ''' File for working on runway stuff'''
+
+import pandas as pd
+import os
+
+runways = []
+
+for folder in os.listdir(os.getcwd() + '\\data\\arrival_flights'):
+    for file in os.listdir(os.getcwd() + '\\data\\arrival_flights\\' + folder):
+        if file[-4:] == '.csv':
+
+            flight = pd.read_csv("data\\arrival_flights\\" + folder + "\\" + file)
+
+            if len(set(flight["runway"])) == 1: # We don't have runway information for all flights. This covers situation where that occurs.
+
+                runway = [set(flight["runway"]), list(flight["timestamp"])[-1]]
+                runways.append(runway)
+                # Dit is dus je runway + de timestamp van landen.
+
+            print(runway)
