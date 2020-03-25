7e487da61237d8e80fd05132a8a4813a44841434
Jelmer <jelmer.dehaan10@gmail.com>
2020-03-25 13:33:28 +0100

    slightly tweaked the output

--- runway_analyser.py
+++ runway_analyser.py
@@ -6,15 +6,15 @@ import os
 runways = []
 
 ## ARRIVALS
-for folder in os.listdir(os.getcwd() + '\\data\\arrival_flights'):
-    for file in os.listdir(os.getcwd() + '\\data\\arrival_flights\\' + folder):
-        if file[-4:] == '.csv':
+for folder in os.listdir(os.getcwd() + '\\data\\arrival_flights'): # Folders
+    for file in os.listdir(os.getcwd() + '\\data\\arrival_flights\\' + folder): # Flights
+        if file[-4:] == '.csv': # Making sure we can do something with this file
 
             flight = pd.read_csv("data\\arrival_flights\\" + folder + "\\" + file)
 
             if len(set(flight["runway"])) == 1: # We don't have runway information for all flights. This covers situation where that occurs.
 
-                runway = [int(flight["runway"][0]), list(flight["timestamp"])[-1]]
+                runway = [int(list(flight["runway"])[-1]), list(flight["timestamp"])[-1]]
                 runways.append(runway)
                 # Dit is dus je runway + de timestamp van landen.
 
