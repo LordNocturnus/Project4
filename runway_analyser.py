835a2ec49495467d804f1f6689b00555719237af
tostijong <brambuijvoets@gmail.com>
2020-03-28 19:07:59 +0100

    poging tot tijd sorteren gedaan

--- runway_analyser.py
+++ runway_analyser.py
@@ -2,11 +2,21 @@
 
 import pandas as pd
 import os
+import numpy as np
 
-runways = []
-runway10 = [["runway", "timestamp", "arriving/departing"]]
-runway14 = [["runway", "timestamp", "arriving/departing"]]
-runway16 = [["runway", "timestamp", "arriving/departing"]]
+def sort_runway_time(runwaylst):
+    dtype = [('runway',int),('date','S19'),('departing/arriving',bool)]
+    values = runwaylst
+    a = np.array(values,dtype=dtype)
+    x = np.sort(a,order='date')
+    return x
+
+
+
+#runways = []
+runway10 = []
+runway14 = []
+runway16 = []
 ## ARRIVALS
 for folder in os.listdir(os.getcwd() + '\\data\\arrival_flights'): # Folders
     for file in os.listdir(os.getcwd() + '\\data\\arrival_flights\\' + folder): # Flights
@@ -16,16 +26,20 @@ for folder in os.listdir(os.getcwd() + '\\data\\arrival_flights'): # Folders
 
             if len(set(flight["runway"])) == 1: # We don't have runway information for all flights. This covers situation where that occurs.
 
-                runway = [int(list(flight["runway"])[-1]), list(flight["timestamp"])[-1], True]
+                runway = (int(list(flight["runway"])[-1]), list(flight["timestamp"])[-1], True)
                 realrunway = runway[0]
+
                 if realrunway == 10 or realrunway == 28:
-                    runway10.append(runway)
+                   runway10.append(runway)
 
-                elif realrunway == 14 or realrunway == 32:
-                    runway14.append(runway)
 
-                elif realrunway == 16 or realrunway == 34:
-                    runway16.append(runway)
+#                elif realrunway == 14 or realrunway == 32:
+#                    runway14.append(runway)
+
+#                elif realrunway == 16 or realrunway == 34:
+#                    runway16.append(runway)
+
+
 
 
                 #runways.append(runway) #runways[i][0] is runway, runways [i][1] is timestamp
@@ -48,22 +62,29 @@ for folder in os.listdir(os.getcwd() + '\\data\\departure_flights'):
                 if realrunway == 10 or realrunway == 28:
                     runway10.append(runway)
 
-                elif realrunway == 14 or realrunway == 32:
-                    runway14.append(runway)
+#                elif realrunway == 14 or realrunway == 32:
+#                    runway14.append(runway)
 
-                elif realrunway == 16 or realrunway == 34:
-                    runway16.append(runway)
+
+#                elif realrunway == 16 or realrunway == 34:
+
+#                    runway16.append(runway)
                 #runways.append(runway)
-                # Dit is dus je runway + de timestamp van landen.
+                # Dit is dus je runway + de timestamp van vertrek.
 
 #            print(runway)
 
-print("runway 10")
-print(runway10)
-print('runway 14')
-print(runway14)
-print("runway 16")
-print(runway16)
+runway10fin = sort_runway_time(runway10)
+#runway14fin = sort_runway_time(runway14)
+#runway16fin = sort_runway_time(runway16)
+
+print('runway10')
+print(runway10fin)
+print(len(runway10fin))
+#print('runway 14')
+#print(runway14)
+#print('runway16')
+#print(runway16)
 
 
 
