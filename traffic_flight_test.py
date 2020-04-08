f07916d88f6925087d040a93c5b9e2fb7779732b
yesn314 <51535967+yesn314@users.noreply.github.com>
2020-04-08 20:54:34 +0200

    First working plots

--- traffic_flight_test.py
+++ traffic_flight_test.py
@@ -4,20 +4,19 @@ import sys
 import os
 import cartopy
 from cartopy import feature
+from cartopy.io.img_tiles import Stamen
 
 land = feature.NaturalEarthFeature(
     "physical", "land", "10m", edgecolor="gray", facecolor="#dddddd", linewidth=0.5
 )
-
 borders = feature.NaturalEarthFeature(
     "cultural",
     "admin_0_boundary_lines_land",
-    "50m",
+    "10m",
     edgecolor="black",
     facecolor="none",
     linestyle=":",
 )
-
 lakes = feature.NaturalEarthFeature(
     "physical",
     "lakes",
@@ -25,114 +24,102 @@ lakes = feature.NaturalEarthFeature(
     facecolor = "b",
     alpha = 0.5
 )
-
 urban = feature.NaturalEarthFeature(
     "cultural",
     "populated_places",
     "10m"
 )
 
+def custom_plot():
+    ax.add_feature(land)
+    ax.add_feature(borders)
+    ax.add_feature(lakes)
+    ax.set_extent([7.54, 9.8, 46.73, 48.16])
+
+def Stamen_terrain_background_plot():
+    tiler = Stamen('terrain-background')
+    ax.set_extent([7.54, 9.8, 46.73, 48.16])
+    zoom = 10
+    ax.add_image(tiler, zoom)
+    ax.add_feature(borders)
+    ax.plot([8.535937,8.556704],[47.475701,47.445563],transform=cartopy.crs.Geodetic(), linewidth= 4, color = 'black')
+
+def Stamen_plot():
+    tiler = Stamen('terrain')
+    ax.set_extent([7.54, 9.8, 46.73, 48.16])
+    zoom = 10
+    ax.add_image(tiler, zoom)
+    ax.add_feature(borders)
+
 ax = plt.subplot(111, projection=cartopy.crs.EuroPP())
-ax.add_feature(land)
-ax.add_feature(borders)
-ax.add_feature(lakes)
-ax.set_extent([6, 10, 45.8, 47.8])
+#custom_plot()
+Stamen_terrain_background_plot()
+#Stamen_plot()
+
 
 def plot_arrival_track(lon, lat):
     ax.plot(lon, lat, transform=cartopy.crs.Geodetic(), color= "b", linewidth=0.4)
+
+def plot_departure_track(lon,lat):
+    ax.plot(lon, lat, transform=cartopy.crs.Geodetic(), color= "r", linewidth=0.4)
+
+
+def all_arrival_flights():
+    for folder in os.listdir(os.getcwd() + f'\\data\\arrival_flights'):
+        if os.path.isdir(os.getcwd() + f"\\data\\arrival_flights\\{folder}"):
+            for file in os.listdir(os.getcwd() + f"\\data\\arrival_flights\\{folder}"):
+                if file[-4:] == ".csv":
+                    arrival_file = pd.read_csv(f"data\\arrival_flights\\{folder}\\{file}").values
+                    lat,lon = arrival_file[:,7],arrival_file[:,8]
+                    plot_arrival_track(lon, lat)
     
 
-#for folder in os.listdir(os.getcwd() + '\\data\arrival_flights'):
-#    if os.path.isdir(f"\\data\arrival_flights\{folder}"):
-#        for file in os.listdir(os.getcwd() + f"\\data\arrival_flights\{folder}"):
-#            if file[-4:] == ".csv":
-#                arrival_file = pd.read_csv(f"data\arrival_flights\{folder}\{file}").values
-#                lat,lon = arrival_file[:,7],arrival_file[:,8]
-#                plot_arrival_track(lon, lat)
-                
+def all_departure_flights():    
+    for folder in os.listdir(os.getcwd() + f'\\data\\departure_flights'):
+        if os.path.isdir(os.getcwd() + f"\\data\\departure_flights\\{folder}"):
+            for file in os.listdir(os.getcwd() + f"\\data\\departure_flights\\{folder}"):
+                if file[-4:] == ".csv":
+                    departure_file = pd.read_csv(f"data\\departure_flights\\{folder}\\{file}").values
+                    lat,lon = departure_file[:,8],departure_file[:,9]
+                    plot_departure_track(lon, lat)
+    
 
-flight_num = 10
+def part_of_arrival_flights():
+    flight_num = 10
 
-arrival_list = os.listdir(os.getcwd() + '\\data\\arrival_flights')
+    arrival_list = os.listdir(os.getcwd() + '\\data\\arrival_flights')
 
-for f in range(0, min(len(arrival_list), flight_num)):
-        if os.path.isdir(f"\\data\\arrival_flights\\{arrival_list[f]}"):
-            print(True)
+    for f in range(0, min(len(arrival_list), flight_num)):
+        print(arrival_list[f])
+        if os.path.isdir(os.getcwd() + f"\\data\\arrival_flights\\{arrival_list[f]}"):
             for file in os.listdir(os.getcwd() + f"\\data\\arrival_flights\\{arrival_list[f]}"):
                 if file[-4:] == ".csv":
                     arrival_file = pd.read_csv(f"data\\arrival_flights\\{arrival_list[f]}\\{file}").values
                     lat,lon = arrival_file[:,7],arrival_file[:,8]
-                    print(lat,lon)
-                    plot_arrival_track(lon, lat)
-        
+                    plot_arrival_track(lon, lat)    
+
+def part_of_departure_flights():
+    flight_num = 10
+
+    departure_list = os.listdir(os.getcwd() + '\\data\\departure_flights')
+
+    for f in range(0, min(len(departure_list), flight_num)):
+        print(departure_list[f])
+        if os.path.isdir(os.getcwd() + f"\\data\\departure_flights\\{departure_list[f]}"):
+            for file in os.listdir(os.getcwd() + f"\\data\\departure_flights\\{departure_list[f]}"):
+                if file[-4:] == ".csv":
+                    departure_file = pd.read_csv(f"data\\departure_flights\\{departure_list[f]}\\{file}").values
+                    lat,lon = departure_file[:,8],departure_file[:,9]
+                    plot_departure_track(lon, lat)    
+    
+
+
+part_of_arrival_flights()
+#part_of_departure_flights()
+
+#all_arrival_flights()
+#all_departure_flights()
+
+#plt.savefig("file.png",bbox = "tight",dpi = 3600)
+
 plt.show()
-                                                                                    
-"""
-df_adsb = pd.read_csv("data/kl1793_adsb.csv")
-df_adsb["alt"] = df_adsb["baroaltitude"] * 3.28084
-df_adsb["spd"] = df_adsb["velocity"] * 1.94384
-t0 = df_adsb["time"].min()
-df_adsb["time"] = df_adsb["time"] - t0
-df_ehs = pd.read_csv("data/kl1793_ehs.csv")
-df_ehs["time"] = df_ehs["time"] - t0
-df_adsb = df_adsb[df_adsb.time < df_ehs.time.max()]
-df_adsb = df_adsb[df_adsb.time > df_ehs.time.min()]
-def plot_track(*args, **kwargs):
-    matplotlib.rc("figure", figsize=(7, 4.326))
-
-    lon, lat = (
-        df_adsb.dropna(subset=["lon"]).lon.values,
-        df_adsb.dropna(subset=["lat"]).lat.values,
-    )
-    ax1 = plt.subplot(111, projection=cartopy.crs.EuroPP())
-    ax1.add_feature(bootstrap.land)
-    ax1.add_feature(bootstrap.borders)
-    ax1.plot(lon, lat, transform=cartopy.crs.Geodetic(), color="k")
-    ax1.text(lon[0] - 1, lat[0] + 0.2, "EHAM", transform=cartopy.crs.Geodetic())
-    ax1.text(lon[-1] + 0.5, lat[-1], "EDDM", transform=cartopy.crs.Geodetic())
-    ax1.set_extent([-2, 17, 46, 55])
-
-    return plt
-save = sys.argv[1] if len(sys.argv) > 1 else False
-plot_track(save=save, name="example_flight_track")
-def main():
-
-    ax = plt.axes(projection=cartopy.crs.PlateCarree())
-
-    ax.add_feature(cartopy.feature.LAND)
-    ax.add_feature(cartopy.feature.OCEAN)
-    ax.add_feature(cartopy.feature.COASTLINE)
-    ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
-    ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
-    ax.add_feature(cartopy.feature.RIVERS)
-
-    ax.set_extent([-20, 60, -40, 40])
-
-    plt.show()
-
-
-if __name__ == '__main__':
-    main()
-
-arrival_data = pd.read_csv(r'D:\Aerospace Engineering\2019-2020 Bsc2\Test Analysis and Simulation\AAL92_20575.csv').values
-departure_data = pd.read_csv(r'D:\Aerospace Engineering\2019-2020 Bsc2\Test Analysis and Simulation\AAL93_19399.csv').values
-arrival_latitude,arrival_longitude = arrival_data[:,7],arrival_data[:,8]
-departure_latitude,departure_longitude = departure_data[:,8],departure_data[:,9]
-
-
-print(arrival_latitude)
-def generate_graph(filename,xs, ys, labels, linelabels):
-    plt.plot(xs[0],ys[0], label=linelabels[0],linewidth=2,color='b')
-    plt.plot(xs[1],ys[1], label=linelabels[1],linewidth=2,color='r')
-    plt.plot([8.535937,8.556704],[47.475701,47.445563], label="ZRH", linewidth= 4, color = 'black')
-    plt.xlabel(labels[0],fontsize="16")
-    plt.ylabel(labels[1],fontsize="16")
-    plt.grid(axis='both')
-    plt.tick_params(axis='both', labelsize="large")
-    plt.legend()
-    plt.savefig(filename+".pdf",bbox_inches="tight")
-    plt.show()
-    plt.close()
-
-generate_graph('test_flights',[arrival_longitude,departure_longitude],[arrival_latitude,departure_latitude],["Longitude","Latitude"],["Arriving","Departing"])
-"""
