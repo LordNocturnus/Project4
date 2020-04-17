1b20f0b12b162a053dd34600c47cbb4c6aa646e7
Jelmer <jelmer.dehaan10@gmail.com>
2020-04-17 14:23:10 +0200

    added initial number of a/c calculator

--- howmanyairplanes.py
+++ howmanyairplanes.py
@@ -25,9 +25,9 @@ def GetDates(lekker):
 
 # Read data and sort them by day
 #aircrafts14 = np.array(pd.read_csv("data\\runway14_32.csv"))
-aircrafts10 = arraytolist(np.array(pd.read_csv("data\\runway10_28.csv")))
-aircrafts14 = arraytolist(np.array(pd.read_csv("data\\runway14_32.csv")))
-aircrafts16 = arraytolist(np.array(pd.read_csv("data\\runway16_34.csv")))
+aircrafts10 = arraytolist(np.array(pd.read_csv("data\\runway_usage\\runway10_28.csv")))
+aircrafts14 = arraytolist(np.array(pd.read_csv("data\\runway_usage\\runway14_32.csv")))
+aircrafts16 = arraytolist(np.array(pd.read_csv("data\\runway_usage\\runway16_34.csv")))
 
 flights = sorted(aircrafts10 + aircrafts14 + aircrafts16, key=lambda aircraftsitem: aircraftsitem[1])
 #sorted(runwaylst, key=lambda runwaylstitem: runwaylstitem[1])
@@ -41,29 +41,53 @@ dates = GetDates(flights)
 
 
 ## determine how many airplanes are initially @zurich
-Ninit = 10
+
+initial_amount = 0
+icao_list = pd.read_csv("data\\icao24.csv", dtype= {'icao24':str, 'arriving':bool})
+
+icaos = set(icao_list['icao24'])
+
+icao_list = icao_list.set_index(['icao24'])
+print(icao_list.loc['0083c3'])
+print(icao_list.loc['0083c3'].iloc[0]['arriving'])
+print(icao_list.loc['0083c3'].shape)
+
+for icao in icaos:
+
+    if len(icao_list.loc[icao].shape) > 1:
+
+        if icao_list.loc[icao]['arriving'].iloc[0] == False:
+            initial_amount += 1
+            print('yes!, ' , icao)
+
+    else:
+
+        if icao_list.loc[icao]['arriving'] == False:
+            initial_amount += 1
+            print('yes2!, ' , icao)
 
 
+print(initial_amount, '/', len(icaos))
 
 # check all airplanes per day, if arriving --> amount+=1 else amount-=1
 
 finallist = []
 dayamount = [Ninit]
 for i in range(len(flights)-1):
-    amount = dayamount[-1]
+    amount = dayamount[i-1]
     flightdatestring = str(flights[i][1])
     flightdate = flightdatestring[0:10]
-    nextflightdatestring = str(flights[i + 1])
+    nextflightdatestring = str(flights[i + 1][1])
     nextflightdate = nextflightdatestring[0:10]
     if flightdate == nextflightdate:
-        if flights[i][2] == True:
+        if flights[i][4] == True:
             amount += 1
             dayamount.append(amount)
         else:
             amount -= 1
             dayamount.append(amount)
-        print(amount)
-    print(dayamount)
+        print('amount:', amount)
+    print('day:' , dayamount)
 
     #daycapacity = max(dayamount)
     #entry = [date,daycapacity]
