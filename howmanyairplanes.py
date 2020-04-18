dea7393e7aff789319a73955327dad1d6021452d
tostijong <brambuijvoets@gmail.com>
2020-04-18 12:03:57 +0200

    finished the program!!!!!whoopwhoop

--- howmanyairplanes.py
+++ howmanyairplanes.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 
 
+
+
 #definitions
 def arraytolist(array):
     lst = []
@@ -14,7 +16,7 @@ def arraytolist(array):
 def GetDates(lekker):
     dates = []
     for i in range(len(lekker)):
-        timestamp = lekker[i][1]
+        timestamp = lekker[i][3]
         string = str(timestamp)
         date = string[0:10]
         dates.append(date)
@@ -29,7 +31,7 @@ aircrafts10 = arraytolist(np.array(pd.read_csv("data\\runway_usage\\runway10_28.
 aircrafts14 = arraytolist(np.array(pd.read_csv("data\\runway_usage\\runway14_32.csv")))
 aircrafts16 = arraytolist(np.array(pd.read_csv("data\\runway_usage\\runway16_34.csv")))
 
-flights = sorted(aircrafts10 + aircrafts14 + aircrafts16, key=lambda aircraftsitem: aircraftsitem[1])
+flights = sorted(aircrafts10 + aircrafts14 + aircrafts16, key=lambda aircraftsitem: aircraftsitem[3])
 #sorted(runwaylst, key=lambda runwaylstitem: runwaylstitem[1])
 dates = GetDates(flights)
 #print(str(flights[0][2]))
@@ -48,9 +50,9 @@ icao_list = pd.read_csv("data\\icao24.csv", dtype= {'icao24':str, 'arriving':boo
 icaos = set(icao_list['icao24'])
 
 icao_list = icao_list.set_index(['icao24'])
-print(icao_list.loc['0083c3'])
-print(icao_list.loc['0083c3'].iloc[0]['arriving'])
-print(icao_list.loc['0083c3'].shape)
+#print(icao_list.loc['0083c3'])
+#print(icao_list.loc['0083c3'].iloc[0]['arriving'])
+#print(icao_list.loc['0083c3'].shape)
 
 for icao in icaos:
 
@@ -58,40 +60,49 @@ for icao in icaos:
 
         if icao_list.loc[icao]['arriving'].iloc[0] == False:
             initial_amount += 1
-            print('yes!, ' , icao)
+            #print('yes!, ' , icao)
 
     else:
 
         if icao_list.loc[icao]['arriving'] == False:
             initial_amount += 1
-            print('yes2!, ' , icao)
+            #print('yes2!, ' , icao)
 
 
-print(initial_amount, '/', len(icaos))
+#print(initial_amount, '/', len(icaos))
 
 # check all airplanes per day, if arriving --> amount+=1 else amount-=1
-
+Ninit = initial_amount
 finallist = []
 dayamount = [Ninit]
-for i in range(len(flights)-1):
-    amount = dayamount[i-1]
-    flightdatestring = str(flights[i][1])
-    flightdate = flightdatestring[0:10]
-    nextflightdatestring = str(flights[i + 1][1])
-    nextflightdate = nextflightdatestring[0:10]
-    if flightdate == nextflightdate:
-        if flights[i][4] == True:
-            amount += 1
-            dayamount.append(amount)
-        else:
-            amount -= 1
-            dayamount.append(amount)
-        print('amount:', amount)
-    print('day:' , dayamount)
+for date in dates:
+    initial_nextday = dayamount[-1]
+    dayamount.clear()
+    dayamount.append(initial_nextday)
+    amount = dayamount[-1]
+    for i in range(len(flights) - 1):
+        nextflightdatestring = str(flights[i + 1][3])
+        nextflightdate = nextflightdatestring[0:10]
+        if date == nextflightdate:
+            if flights[i][4] == True:
+                amount += 1
+                dayamount.append(amount)
+            elif flights[i][4] == False:
+                amount -= 1
+                if amount < 0:
+                    amount = 0
+                dayamount.append(amount)
+            # print('amount:', amount)
+    daycapacity = max(dayamount)
+    entry = [date,daycapacity]
+    finallist.append(entry)
 
-    #daycapacity = max(dayamount)
-    #entry = [date,daycapacity]
-    #finallist.append(entry)
 
-#print(finallist)
-#print(len(finallist))
+panda_columns = ['date', 'maximum_amount_of_airplanes']
+
+finallistpanda = pd.DataFrame(finallist, columns=panda_columns)
+finallistpanda.to_csv("data\\runway_usage\\howmanyairplanes.csv",index=False)
+print('done')
+
+
+
