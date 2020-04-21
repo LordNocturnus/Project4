18fda4e5c158df7940b7eae294f2cf644edea3e8
LordNocturnus <41686329+LordNocturnus@users.noreply.github.com>
2020-04-21 17:10:25 +0200

    added min_max calculater

--- kmeans_single_flight.py
+++ kmeans_single_flight.py
@@ -22,16 +22,17 @@ def colors(n, colorshift):
         h += step
     return ret
 
-def Kmeans_single_flight(filelocation,N,list_of_Parameter,plot=True,return_cents=False,return_colors_rgb=False,colorshift=90):
 
+def Kmeans_single_flight(filelocation, N, list_of_Parameter, plot=True, return_cents=False, return_colors_rgb=False,
+                         colorshift=90):
     # initial values & setup
     C = "empty"
     N = N
     file = filelocation
     df = pd.read_csv(file)
-    #scaledata = df[["altitude","geoaltitude","groundspeed","latitude","longitude","track","vertical_rate","distance","runway"]]
+    # scaledata = df[["altitude","geoaltitude","groundspeed","latitude","longitude","track","vertical_rate","distance","runway"]]
     scaledata = df[list_of_Parameter]
-    df_scale = pd.DataFrame(scale(scaledata),columns=scaledata.columns)
+    df_scale = pd.DataFrame(scale(scaledata), columns=scaledata.columns)
 
     # Kmeans
     km = KMeans(n_clusters=N)
@@ -40,7 +41,7 @@ def Kmeans_single_flight(filelocation,N,list_of_Parameter,plot=True,return_cents
 
     # execute color function
     if return_colors_rgb:
-        colours = colors(N,colorshift)
+        colours = colors(N, colorshift)
         for i in range(0, np.size(df.cluster)):
             if C == "empty":
                 C = np.array(list(colours[df.cluster[i]]))
@@ -57,12 +58,12 @@ def Kmeans_single_flight(filelocation,N,list_of_Parameter,plot=True,return_cents
         fig = plt.figure()
         ax = Axes3D(fig)
         if C == "empty":
-            colours = colors(N,colorshift)
-            for i in range(0,np.size(df.cluster)):
+            colours = colors(N, colorshift)
+            for i in range(0, np.size(df.cluster)):
                 if C == "empty":
                     C = np.array(list(colours[df.cluster[i]]))
                 else:
-                    C = np.vstack((C,list(colours[df.cluster[i]])))
+                    C = np.vstack((C, list(colours[df.cluster[i]])))
 
         ax.scatter([df.latitude], [df.longitude], [df.altitude], c=C)
         ax.set_xlabel("latitude")
