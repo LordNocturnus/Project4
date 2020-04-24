b48f7cc5145637bab9859b1e8da4dc0c8f224671
Lenny <61494250+lpfdegraaf-tudelft@users.noreply.github.com>
2020-04-24 14:17:18 +0200

    Update Failed_Landings.py

--- Failed_Landings.py
+++ Failed_Landings.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 
 failed_landings = []
+failed_landings_Codes = []
 
 for folder in os.listdir(os.getcwd() + f"\\data\\arrival_flights"):
     if os.path.isdir(os.getcwd() + f"\\data\\arrival_flights\\{folder}"):
@@ -20,25 +21,32 @@ for folder in os.listdir(os.getcwd() + f"\\data\\arrival_flights"):
                 for i in range(len(geoalt)):
                     if arrival_file[0,-2] == 'YUPZM_19073':
                         print(failed_landings)
+                        failed_landings_Codes = list(dict.fromkeys(failed_landings_Codes))
+                        print(failed_landings_Codes)
                         print('done')
                         quit()
 
-                    elif geoalt[i] <= 2200 and geoalt[i] != geoalt[-1] : # to check if it is close to landing but not landed yet
+                    elif geoalt[i-1] <= 3000 and arrival_file[i-1,0] != arrival_file[-1,0] : # to check if it is close to landing but not landed yet
                         #if geoalt[i + 1] <= geoalt[i] :  # check if it is still descending or level
                             #okay
                             #print('Descending')
-                        if geoalt[i] > geoalt[i-1] and geoalt[i] != geoalt[-1]:
-                            if geoalt[i + 1] > geoalt[i] and geoalt[i + 1] != geoalt[-1] and geoalt[i + 2] > geoalt[i + 1] and geoalt[i + 2] != geoalt[-1]:
+                        if geoalt[i] > geoalt[i-1] and arrival_file[i,0] != arrival_file[-1,0]:
+                            if geoalt[i + 1] > geoalt[i] and arrival_file[i+1,0] != arrival_file[-1,0]:
                                 #print('failed landing attempt')
                                 #print(arrival_file[0,-2])
                                 #print(arrival_file[0,2])
                                 #print(arrival_file[i+3,0])
                                 #if 'arrival_file[0.2]' not in failed_landings:
                                 #if arrival_file[i,:] not in failed_landings:
-                                #failed_landings.append(arrival_file[i,:]) #for the full row of the timestep
-                                failed_landings.append(arrival_file[0,2])
-                        elif geoalt[i + 1] == geoalt[-1]:
+                                failed_landings.append(arrival_file[i,:])
+                                failed_landings_Codes.append(arrival_file[0,-2])
+
+
+                        elif geoalt[i] == geoalt[-1] and arrival_file[i,0] == arrival_file[-1,0]:
                             break
+
+
 #def Zurich_Altitude_check
-    #Geo_Altitude_zurich = roughly 1417 feet
-    #Range of minimum geoaltitudes recorded at landing: 1850 -- 1450 'feet'
+#Geo_Altitude_zurich = roughly 1417 feet
+#Range of minimum geoaltitudes recorded at landing: 1850 -- 1450 'feet'
+
