c0718ab632c5d76548f3e32890052ff6752c813a
Jelmer <jelmer.dehaan10@gmail.com>
2020-05-02 16:22:14 +0200

    initial calculations done for actualConcept.py

--- actualConcept.py
+++ actualConcept.py
@@ -8,4 +8,23 @@ runway16_34 = pd.read_csv("data\\runway_usage\\runway16_34.csv", dtype = {'icao2
 
 print('import ok')
 
-if runway10_28['arriving'] == True:
+probable_concept = []
+
+for i, row in runway14_32.iterrows():
+    if row['arriving'] == True:
+        probable_concept.append([row['timestamp'], 0])
+
+for i, row in runway10_28.iterrows():
+    if row['arriving'] == True:
+        probable_concept.append([row['timestamp'], 1])
+
+for i, row in runway16_34.iterrows():
+    if row['arriving'] == True:
+        probable_concept.append([row['timestamp'], 2])
+
+print('concepts analysed')
+
+pandacolumns = ['timestamp', 'concept']
+
+x = pd.DataFrame(probable_concept, columns=pandacolumns).sort_values(by="timestamp")
+x.to_csv("data\\actual_concept.csv", index=False)
