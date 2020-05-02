186c0303a769eb5aa9012866009a71d8ec720ac0
Jelmer <jelmer.dehaan10@gmail.com>
2020-05-02 15:59:21 +0200

    fixed day of week in ExpectedConcept.py

--- ExpectedConcept.py
+++ ExpectedConcept.py
@@ -27,23 +27,20 @@ dates = GetDates(newflights['date'])
 lst2 = []
 for i in range(len(dates)):
     date = dates[i]
-    if i <= 6:
-        lst2.append([date,days[i]])
-    else:
-        if i % 6 == 0: #and lst2[i-1][1] is not 'tuesday':
-            lst2.append([date,'tuesday'])
-        elif i % 7 == 0: #and lst2[i-1][1] is not 'wednesday':
-            lst2.append([date,'wednesday'])
-        elif i % 8 == 0: #and lst2[i-1][1] is not 'thursday':
-            lst2.append([date,'thursday'])
-        elif i % 9 == 0: #and lst2[i-1][1] is not 'friday':
-            lst2.append([date,'friday'])
-        elif i % 10 == 0: #and lst2[i-1][1] is not 'saturday':
-            lst2.append([date,'saturday'])
-        elif i % 11 == 0: #and lst2[i-1][1] is not 'sunday':
-            lst2.append([date,'sunday'])
-        elif i % 12 == 0: #and lst2[i-1][1] is not 'monday':
-            lst2.append([date,'monday'])
+    if i % 7 == 0: #and lst2[i-1][1] is not 'tuesday':
+        lst2.append([date,'tuesday'])
+    elif i % 7 == 1: #and lst2[i-1][1] is not 'wednesday':
+        lst2.append([date,'wednesday'])
+    elif i % 7 == 2: #and lst2[i-1][1] is not 'thursday':
+        lst2.append([date,'thursday'])
+    elif i % 7 == 3: #and lst2[i-1][1] is not 'friday':
+        lst2.append([date,'friday'])
+    elif i % 7 == 4: #and lst2[i-1][1] is not 'saturday':
+        lst2.append([date,'saturday'])
+    elif i % 7 == 5: #and lst2[i-1][1] is not 'sunday':
+        lst2.append([date,'sunday'])
+    elif i % 7 == 6: #and lst2[i-1][1] is not 'monday':
+        lst2.append([date,'monday'])
 
 
 
