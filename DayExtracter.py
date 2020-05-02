5e8661376227dea192842e944654ae5036d7b8df
tostijong <brambuijvoets@gmail.com>
2020-05-02 19:05:06 +0200

    made day extracter

---
+++ DayExtracter.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+def GetDates(lekker):
+    dates = []
+    for i in range(len(lekker)):
+        dates.append(lekker[i])
+    y = sorted(set(dates))
+    return y
+
+flights = pd.read_csv("data\\useful_flights.csv", dtype= {'icao24':str, 'arriving':bool}).sort_values(by=['timestamp'])
+
+lst = []
+for i,flight in flights.iterrows():
+    date = flight['timestamp'][0:10]
+    time = flight['timestamp'][11:19]
+    entry = [date,time]
+    lst.append(entry)
+
+newcolumns = ['date','time']
+newflights = pd.DataFrame(lst,columns=newcolumns)
+
+
+#1 oktober=dinsdag
+days = ['tuesday','wednesday','thursday','friday','saturday','sunday','monday']
+dates = GetDates(newflights['date'])
+lst2 = []
+for i in range(len(dates)):
+    date = dates[i]
+    if i % 7 == 0:
+        lst2.append([date,'tue'])
+    elif i % 7 == 1:
+        lst2.append([date,'wed'])
+    elif i % 7 == 2:
+        lst2.append([date,'thu'])
+    elif i % 7 == 3:
+        lst2.append([date,'fri'])
+    elif i % 7 == 4:
+        lst2.append([date,'sat'])
+    elif i % 7 == 5:
+        lst2.append([date,'sun'])
+    elif i % 7 == 6:
+        lst2.append([date,'mon'])
+
+dayscolumns = ['date', 'day']
+daysDF = pd.DataFrame(lst2,columns=dayscolumns)
+#print(daysDF)
+lst3 = []
+for i, time in daysDF.iterrows():
+    date = time['date']
+    day = time['day']
+    for j, flight in newflights.iterrows():
+        if date == flight['date']:
+            lst3.append([flight['date'], day, flight['time']])
+
+lastcolumns = ['date', 'day', 'timestamp']
+panda = pd.DataFrame(lst3, columns=lastcolumns)
+panda.to_csv("data\\addeddayforexpectedconcept.csv", index=False)
+print('ddone')
---
+++ data/ExpectedConcept.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+flights = pd.read_csv("data\\addeddayforexpectedconcept.csv")
+
+print(flights)
