1cb2348038dbf3b67e0747733f1bb93fed1f2d7c
Jelmer <jelmer.dehaan10@gmail.com>
2020-05-03 19:33:52 +0200

    completed first analysis for actualConcept.py. Now moving on to finding switch moments.

--- actualConcept.py
+++ actualConcept.py
@@ -9,22 +9,54 @@ runway16_34 = pd.read_csv("data\\runway_usage\\runway16_34.csv", dtype = {'icao2
 print('import ok')
 
 probable_concept = []
+errors = 0
 
 for i, row in runway14_32.iterrows():
-    if row['arriving'] == True:
-        probable_concept.append([row['timestamp'], 0])
+    if row['arriving'] == True and row['runway'] == 14:
+        concept = 0
+    elif row['arriving'] == False and row['runway'] == 32:
+        concept = 12
+    #elif row['arriving'] == False:
+    #    concept = 99
+    else:
+        concept = 99
+        errors += 1
+    runway14_32.loc[i,'concept'] = concept
+
+print('analysed runway 14/32')
 
 for i, row in runway10_28.iterrows():
-    if row['arriving'] == True:
-        probable_concept.append([row['timestamp'], 1])
+    if row['arriving'] == True and row['runway'] == 28:
+        concept = 1
+    elif row['arriving'] == False and row['runway'] == 10:
+        concept = 0
+    elif row['arriving'] == False:
+        concept = 0.12
+    else:
+        concept = 99
+        errors += 1
+    runway10_28.loc[i,'concept'] = concept
+
+print('analysed runway 10/28')
 
 for i, row in runway16_34.iterrows():
-    if row['arriving'] == True:
-        probable_concept.append([row['timestamp'], 2])
+    if row['arriving'] == True and row['runway'] == 34:
+        concept = 2
+    elif row['arriving'] == False and row['runway'] == 16:
+        concept = 0
+    elif row['arriving'] == False:
+        concept = 12
+    else:
+        concept = 99
+        errors += 1
+    runway16_34.loc[i,'concept'] = concept
 
-print('concepts analysed')
+print('analysed runway 16/34')
 
-pandacolumns = ['timestamp', 'concept']
+print(f'concepts analysed with {errors} errors.')
 
-x = pd.DataFrame(probable_concept, columns=pandacolumns).sort_values(by="timestamp")
-x.to_csv("data\\actual_concept.csv", index=False)
+probable_concept = runway16_34.append(runway10_28).append(runway14_32).sort_values(by="timestamp")
+
+#x = pd.DataFrame(probable_concept).sort_values(by="timestamp")
+probable_concept.to_csv("data\\actual_concept.csv", index=False)
+
